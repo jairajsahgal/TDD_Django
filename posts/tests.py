4e90d001a493a4e433b6b4f226903bf99639beaa
Jairaj Sahgal <jairajiitaspirant@gmail.com>
2023-08-21 00:27:59 +0530

    Added templates in settings Added posts url in project routing. Registered post model in admin panel. Added string representation of Post model. Created test for string representation and template response Added urls in posts app. Added homepage view. Added template for homepage.

--- posts/tests.py
+++ posts/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from posts.models import Post
+import string
+from http import HTTPStatus
+import secrets
 # Create your tests here.
 
 class PostModelTest(TestCase):
@@ -7,3 +10,27 @@ class PostModelTest(TestCase):
         posts = Post.objects.all().count()
 
         self.assertEqual(posts,0)
+    
+    def test_string_rep_of_objects(self):
+
+        post = Post.objects.create(
+            title="Test Body",
+            body="Test Body"
+        )
+
+        self.assertEqual(str(post),post.title)
+
+
+class HomepageTest(TestCase):
+    def setUp(self) -> None:
+        for i in range(10):
+            characters = string.ascii_letters + string.digits
+            title = ''.join(secrets.choice(characters) for _ in range(10))
+            body = ''.join(secrets.choice(characters) for _ in range(200))
+            Post.objects.create(title=title,body=body)
+        
+    def test_homepage_returns_correct_response(self):
+        response = self.client.get('/')
+
+        self.assertTemplateUsed(response,'posts/index.html')
+        self.assertEqual(response.status_code,HTTPStatus.OK)
